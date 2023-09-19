d40bc516cb3f39c1bdd70bb97a4817a303dd90fd
fernandoallisson <fernando.alisson@ufpe.br>
2023-09-19 17:09:10 -0300

    Resolução do 19º desafio

--- desafio19.sql
+++ desafio19.sql
@@ -1,1 +1,3 @@
-SELECT COUNT(employee_id IN (5, 6) AND shipper_id = 2) AS orders_count FROM orders;
+SELECT COUNT(*) AS orders_count
+FROM orders
+WHERE (employee_id = 5 OR employee_id = 6) AND shipper_id = 2;
