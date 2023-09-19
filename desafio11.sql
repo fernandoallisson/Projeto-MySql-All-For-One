339519da51892ff0c4b51cfac88c3a4221b4dbc3
fernandoallisson <fernando.alisson@ufpe.br>
2023-09-19 16:56:53 -0300

    Resolução do 11º desafio

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,3 @@
 SELECT notes
 FROM purchase_orders
-WHERE notes LIKE '%Purchase generated based on Order #3%' OR
-      notes LIKE '%Purchase generated based on Order #4%' OR
-      notes LIKE '%Purchase generated based on Order #5%' OR
-      notes LIKE '%Purchase generated based on Order #6%' OR
-      notes LIKE '%Purchase generated based on Order #7%' OR
-      notes LIKE '%Purchase generated based on Order #8%' OR
-      notes LIKE '%Purchase generated based on Order #9%';
+WHERE notes LIKE '%Purchase generated based on Order #3%';
